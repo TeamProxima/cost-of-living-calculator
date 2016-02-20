86536e46a79aeab9ce280eed0689a67e63ce1d57
DOGANALP ERGENC <e1881192@ceng.metu.edu.tr>
2016-02-20 13:22:39 +0200

    v1.1 no settings

--- DjangoApp/hackathon/urls.py
+++ DjangoApp/hackathon/urls.py
@@ -3,8 +3,9 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'hackathon.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^home$', 'src.views.home', name='home'),
+    url(r'^questions', 'src.views.run', name='run'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- DjangoApp/src/views.py
+++ DjangoApp/src/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect,render
+from django.template import RequestContext, loader
+from django.core.context_processors import csrf
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.sessions import *
+from django.http import HttpResponse
+from json import dumps, loads, JSONEncoder, JSONDecoder
+from datetime import datetime
+from random import randint
+from .models import *
 
-# Create your views here.
+import time
+
+
+def home(request):
+    '''Show country and city selection'''
+    try:
+        context =  {'message': ''}
+        return render(request, 'index.html', context)
+    except Exception as e:
+        print e
+        return redirect("/home")
+
+
+def run(request):
+    '''Ask questions to calculate price'''
+    try:
+        country = request.GET['country']
+        city = request.GET['city']
+        print country, city
+
+        return render(
+            request, 'questions.html',
+            {'message':'CAT1',
+             'questions':[{'type': 0, 'text': "amina koyyim", 'alt':['DENEME', 'DENEME1']},
+                          {'type': 2, 'text': "yeni bir soru daha", 'opt':['BENI SEC', 'BENI DE SEC']},
+                          {'type': 1, 'text': "rack this bitch"}]})
+    except Exception as e:
+        print e
+        return redirect("/home")
