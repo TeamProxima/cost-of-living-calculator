2a9d3d0589f23bad1701af766c4152025e21160b
Koding <blacksimit@blacksimit.koding.io>
2016-02-20 15:55:59 -0600

    First commit

---
+++ deneme/settings.py
@@ -0,0 +1,102 @@
+"""
+Django settings for deneme project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'wx3-$6=&d^dd)g8o2_g0$16)+_v5!#&!c&u7#d4-3ndl%p#mja'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'deneme.urls'
+
+WSGI_APPLICATION = 'deneme.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+	'NAME': 'test',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+	}
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+    '/home/blacksimit/tmp/deneme/templates',
+)
+ 
+STATIC_URL = '/static/'
+ 
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+ 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, STATIC_URL),
+    '/home/blacksimit/tmp/deneme/static/',
+)
---
+++ src/functions.py
@@ -0,0 +1,72 @@
+def meal_cost(meal_types, veg_ratio, in_ratio, restaurant_ratio):
+    """
+    meal_types: string array containing some of "Breakfast", "Lunch" and "Dinner"
+    veg_ratio: ratio of eating vegatables
+    in_ratio: ratio of eating at house
+    restaurant_ratio: ratio of eating at restaurant
+    """
+
+    inexp_rest = 15.0
+    fastfood_rest = 15.0
+    coke = 2.55
+
+    milk = 2.6
+    bread = 1.27
+    rice = 4.42
+    egg = 5.67
+    cheese = 15.39
+    chicken = 12.66
+    beef = 42.81
+    apple = 3.23
+    banana = 5.74
+    orange = 3.24
+    tomato = 3.16
+    potato = 2.93
+    onion = 2.24
+    lettuce = 1.94
+    water = 1.19
+
+    total = 0
+
+    breakfast_at_home = milk / 4 + bread / 5 + egg / 12 + cheese / 20 + water / 3
+    breakfast_at_outside = breakfast_at_home * 2
+
+    if veg_ratio < 50:
+        dinner_at_home = bread / 5 + rice / 10 + (1-veg_ratio)*(chicken + beef) / 12 + veg_ratio*(tomato + potato + onion + lettuce) / 20 + water
+    else:
+        dinner_at_home = bread / 5 + rice / 10 + (1-veg_ratio)*(chicken + beef) / 12 + 4*veg_ratio*(tomato + potato + onion + lettuce) / 20 + water
+
+    dinner_at_outside = (inexp_rest + coke) * restaurant_ratio + fastfood_rest * (1 - restaurant_ratio)
+
+    lunch_at_home = dinner_at_home * 0.8
+    lunch_at_outside = dinner_at_outside * 0.8
+
+    for meal in meal_types:
+        if meal == "Breakfast":
+            total += in_ratio * breakfast_at_home + (1 - in_ratio) * breakfast_at_outside
+        elif meal == "Lunch":
+            total += in_ratio * lunch_at_home + (1 - in_ratio) * lunch_at_outside
+        elif meal == "Dinner":
+            total += in_ratio * dinner_at_home + (1 - in_ratio) * dinner_at_outside
+
+    fruit_cost = (apple + banana + orange) * 0.1
+    total += fruit_cost
+
+    return total
+
+def beer_cigarette_cost(beer_count,cigarette_count):
+    d_beer_res = 8.5
+    i_beer_res = 10.0
+
+    d_beer_market = 5.58
+    i_beer_market = 7.64
+
+    cigarette = 10.0
+
+    total = ((d_beer_market + d_beer_res + (i_beer_market + i_beer_res)*5/3.3)/4)*beer_count/7
+    total += cigarette*cigarette_count/7
+
+    return total
+
+print meal_cost(["Breakfast","Lunch","Dinner"], 0.7, 0.3, 0.7)
+print beer_cigarette_cost(1,4)
