0e713d87c7d80c266b1e903acf2f7ca076abbc7f
DOGANALP ERGENC <e1881192@ceng.metu.edu.tr>
2016-02-21 11:39:35 +0200

    v1.5 working frontend

--- DjangoApp/src/views.py
+++ DjangoApp/src/views.py
@@ -32,20 +32,27 @@ def run(request):
     questions = [{'id': 'mealQ1', 'type': 4, 'text': "What meals do you have?", 'alt':['Breakfast', 'Launch', 'Dinner']},
           {'id': 'mealQ2', 'type': 3, 'text': "What is your preference for meal?", 'range': range(100), 'alt':['Vegetable', 'Meat']},
           {'id': 'mealQ3', 'type': 1, 'text': "How often do you go out for meal?", 'alt':['times a week']},
-          {'id': 'mealQ4', 'type': 3, 'text': "What do you prefer?", 'range': range(100)} #, alt:['Restaurant', 'Fast Food']},
+          {'id': 'mealQ4', 'type': 3, 'text': "What do you prefer?", 'range': range(100), 'alt': ['Restaurant', 'Fast Food']}
           ]
-    questions2 = [{'id': 'beer_cigQ1', 'type': 1, 'text': "How many bottles of beer do you drink per week?"},
-          {'id': 'beer_cigQ2', 'type': 1, 'text': "How many packets of cigarette do you smoke per week?"}
+    questions2 = [{'id': 'beer_cigQ1', 'type': 1, 'text': "How many bottles of beer do you drink per week?", 'alt':[' ']},
+          {'id': 'beer_cigQ2', 'type': 1, 'text': "How many packets of cigarette do you smoke per week?", 'alt':[' ']}
           ]
 
+    qlist = [questions, questions2]
+    page_index = 0
     try:
         if request.META['HTTP_REFERER'].split('/')[-1] == '':
             '''For country and city selection after index'''
             country = request.GET['country']
             city = request.GET['city']
+        if request.POST:
+            page_index += 1
+            print request.POST
+        print page_index
         return render(request, 'questions.html',
                     {'message': '',
-                    'questions': questions})
+                    'questions': qlist[page_index]})
     except Exception as e:
         print e
+        page_index = 0
         return redirect("/")
